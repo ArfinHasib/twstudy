48d7cb64fceded4436bfddb1b53e12c113cc9325
ArfinHasib <arfinhasib21375@gmail.com>
2024-01-14 22:49:40 +0600

    modal close button and video modal functionality

--- js/main.js
+++ js/main.js
@@ -11,3 +11,23 @@ function testimonialSlider() {
 }
 
 testimonialSlider();
+
+// course preview video
+function coursePreview() {
+   const coursePreviewModal = document.querySelector(
+      '.js-course-preview-modal'
+   );
+   if (coursePreviewModal) {
+      // if the element exists
+      coursePreviewModal.addEventListener('shown.bs.modal', function () {
+         this.querySelector('.js-course-preview-video').play();
+         this.querySelector('.js-course-preview-video').currentTime = 0;
+      });
+
+      coursePreviewModal.addEventListener('hide.bs.modal', () => {
+         this.querySelector('.js-course-prevew-video').pause();
+      });
+   }
+}
+
+coursePreview();
